10d17eaced3be546e55d38af87179473ec0a7d3a
lauriDev123 <lauriellenjepang@gmail.com>
2023-04-02 19:13:51 +0200

    create the login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SplashScreenComponent } from './components/splash-screen/splash-screen.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SplashScreenComponent
+    SplashScreenComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
