cb1483b55ec0c8ebde5b2605f22e307c6c8e1742
BriceNogue <93254397+BriceNogue@users.noreply.github.com>
2023-04-05 00:38:01 +0200

    Create user module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,18 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SplashScreenComponent } from './components/splash-screen/splash-screen.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './components/home/home.component';
+import { AddUserComponent } from './components/user-module/add-user/add-user.component';
+import { UserListComponent } from './components/user-module/user-list/user-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SplashScreenComponent,
     NavBarComponent,
-    HomeComponent
+    HomeComponent,
+    AddUserComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
