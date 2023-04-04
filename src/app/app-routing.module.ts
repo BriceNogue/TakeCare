d1862791a81943681e3c59d7cfbc5f73e7a57c69
BriceNogue <93254397+BriceNogue@users.noreply.github.com>
2023-04-05 00:41:27 +0200

    Add users roads

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AddUserComponent } from './components/user-module/add-user/add-user.component';
+import { UserListComponent } from './components/user-module/user-list/user-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home", component: HomeComponent},
+  {path:"add_user", component: AddUserComponent},
+  {path:"user_list", component: UserListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
