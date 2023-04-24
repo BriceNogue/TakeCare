bfa32eca715668630af512a683fb2db05570c751
lauriDev123 <lauriellenjepang@gmail.com>
2023-04-24 14:46:46 +0200

    Replace the search icon

---
+++ src/app/services/patient-service.service.ts
@@ -0,0 +1,64 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, Observable, of, tap } from 'rxjs';
+import { PatientModel } from '../models/patient-model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PatientService {
+
+  constructor(private http: HttpClient) { }
+
+  private log (response: any) {
+    console.table(response);
+  }
+
+  private handleError(error: Error, errorValue: any) {
+    console.error(error);
+    return of(errorValue);
+  }
+
+  addPatient(patient: PatientModel): Observable<PatientModel> {
+    const httpOptions = {
+      headers: new HttpHeaders({'Content-type': 'application/json'})
+    };
+
+    return this.http.post<PatientModel>('',patient, httpOptions).pipe(
+      tap((response) => this.log(response)),
+      catchError((error) => this.handleError(error, null))
+    );
+  }
+
+  getPatients(): Observable<PatientModel[]> {
+    return this.http.get<PatientModel[]>('').pipe(
+      tap((response) => this.log(response)),
+      catchError((error) => this.handleError(error, []))
+    );
+  }
+
+  getPatientById(patientId: number): Observable<PatientModel|undefined> {
+    return this.http.get<PatientModel>(`/${patientId}`).pipe(
+      tap((response) => this.log(response)),
+      catchError((error) => this.handleError(error, []))
+    );
+  }
+
+  updatePatient(patient: PatientModel): Observable<null> {
+    const httpOptions = {
+      headers: new HttpHeaders({'Content-type': 'application/json'})
+    };
+
+    return this.http.put('', patient, httpOptions).pipe(
+      tap((response) => this.log(response)),
+      catchError((error) => this.handleError(error, null))
+    );
+  }
+
+  deletePatient(patientId: number): Observable<null> {
+    return this.http.delete(`/${patientId}`).pipe(
+      tap((response) => this.log(response)),
+      catchError((error) => this.handleError(error,null))
+    );
+  }
+}
