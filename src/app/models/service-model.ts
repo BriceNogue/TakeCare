2e59c11d5df0632135e3745e2660766bc9d8ada1
BriceNogue <93254397+BriceNogue@users.noreply.github.com>
2023-04-24 21:28:00 +0200

    service model

--- src/app/models/service-model.ts
+++ src/app/models/service-model.ts
@@ -1,3 +1,12 @@
 export class ServiceModel {
-    
+    _id: string;
+    service_code: string;
+    libelle: string;
+    description: string
+
+    constructor( service_code: string ="", libelle: string ="", description: string ="") {
+        this.service_code = service_code;
+        this.libelle = libelle;
+        this.description = description;
+    }
 }
